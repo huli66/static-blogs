548a4aacd8dafe86f8f41d2187dfef96fb79e588
jianjun.hu <jianjun.hu@quantinfotech.com>
2023-02-09 16:43:37 +0800

    修改后端部署命令

--- deploy/blog-backend.sh
+++ deploy/blog-backend.sh
@@ -17,6 +17,14 @@ docker pull huli66/blog-backend:latest
 
 # 运行新的容器，容器的 4090 端口映射到本机的 3090 端口
 echo -e "----------docker Run----------"
-docker run --rm -d -p 3090:4090 --name backend huli66/blog-backend:latest
+docker container run -d -p 3090:4090 --rm --name backend  --volume "$PWD/static-blogs/blogs":/app/blogs huli66/blog-backend:latest
+
+# docker container run
+# -d # 在后台运行
+# -p 3090:4090 # 容器的 4090 端口映射到本机的 3090 端口
+# --rm # 容器停止后自动删除容器文件
+# --name # 容器的名字
+# --volume "$PWD/../blogs":/blogs # 映射本地目录到容器指定目录
+# huli66/blog-backend:latest # 镜像名
 
 echo -e "----------deploy Success----------"
